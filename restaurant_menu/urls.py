35a7985bb7c74236580c493808a5dcc4b89af28f
sunning <ning.sssun@gmail.com>
2024-07-18 17:04:53 +0400

    implement item page

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home')
+    path('', views.MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name="menu_item")
 ]
 
